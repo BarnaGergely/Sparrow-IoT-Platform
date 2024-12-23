b45cdb15a2a31d4f6492ae022e81a7ca229adc30
Barna Gergely <h144988@stud.u-szeged.hu>
2024-12-23 15:56:22 +0100

    add siemens styles and packages

--- src/webapp.client/src/app/app.module.ts
+++ src/webapp.client/src/app/app.module.ts
@@ -2,6 +2,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Siemens Ix Angular Modules
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IxModule } from '@siemens/ix-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -11,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule, AppRoutingModule, IxModule.forRoot(), BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
