329bf26055f8e735422fc9b6701466268f13acff
Barna Gergely <h144988@stud.u-szeged.hu>
2024-12-23 14:39:11 +0100

    Move files to folder and rebuild web templates

--- src/WebApp.Server/Program.cs
+++ src/WebApp.Server/Program.cs
@@ -22,7 +22,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/api/app/weatherforecast", () =>
+app.MapGet("/weatherforecast", () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
